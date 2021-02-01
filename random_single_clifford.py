66745f33fd220e8d4c83295115f77f5fc2745817
Tom Swift <thomas.swift.20@ucl.ac.uk>
2021-02-01 14:11:16 +0000

    Added function to create a Random Clifford sequence

--- random_single_clifford.py
+++ random_single_clifford.py
@@ -2,9 +2,26 @@ from qutip.operators import qeye
 from qutip.qip.gates import rx,ry
 from numpy import pi
 import random
+from qutip.states import basis
+from numpy import pi
+
 
 def random_single_clifford():
     cliff_gates = [qeye(2),rx(pi/2),ry(pi/2),rx(pi),ry(pi)]
     rand_cliff = random.choice(cliff_gates)
     
     return rand_cliff
+
+
+def random_clifford_sequence(n_gates):
+    
+    sequence = [None]*n_gates
+    final = qeye(2)
+    for i in range(n_gates-1):
+        a_random_clifford = random_single_clifford()
+        sequence[i] = a_random_clifford
+        final = a_random_clifford*final
+
+    sequence[n_gates-1] = final.inv()
+    
+    return sequence
