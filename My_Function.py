35279de8e942aa125792eef2126beac0b452d82d
itsasoblg <itsasoblg@hotmail.com>
2021-02-05 10:00:15 +0000

    Updated calculation for x and y gates

---
+++ My_Function.py
@@ -0,0 +1,82 @@
+from numpy import cos,sin,pi,sqrt
+from qutip import *
+import numpy as np
+
+hbar = 1
+w_1 = 65*10**6
+
+def spin_hamiltonian(delta,phi):
+    
+    a = -(hbar/2)*delta*sigmaz()
+    b = -(hbar*w_1/4)*cos(phi)*sigmax()
+    c = (hbar*w_1/4)*sin(phi)*sigmay()
+    
+    H = a + b + c
+    
+    return H
+
+def hadamard_evolution(delta):
+    plus_state = (basis(2,0)+basis(2,1))/sqrt(2)
+    
+    #Ry by pi/2 rotation
+    psi = basis(2,0)
+    phi = pi/2
+    theta = pi/2
+    state1 = general_evolution(psi,theta,phi,delta)
+    
+    #Rx by pi rotation
+    phi = 0
+    theta = pi
+    final_state = general_evolution(state1,theta,phi,delta)
+    
+    fid = fidelity(plus_state*plus_state.dag(),final_state*final_state.dag())
+    
+    return fid
+
+def z_evolution(delta):
+    
+    test_state = 1j*basis(2,0)
+    
+    #R(-X) by pi/2 rotation
+    psi = basis(2,0)
+    phi = pi
+    theta = pi/2
+    state1 = general_evolution(psi,theta,phi,delta)
+    
+    #RY by pi rotation
+    phi = pi/2
+    theta = pi
+    state2 = general_evolution(state1,theta,phi,delta)
+    
+    #Rx by pi/2 rotation
+    phi = 0
+    theta = pi/2
+    final_state = general_evolution(state2,theta,phi,delta)
+    
+    fid = fidelity(test_state*test_state.dag(),final_state*final_state.dag())
+    
+    return fid
+
+def general_evolution(psi,theta,phi,delta):
+    
+    ev_time = 2*theta/w_1
+    H = spin_hamiltonian(delta,phi)
+    t = np.linspace(0,ev_time,10)
+    
+    result = mesolve(H,psi,t)
+    final_state = result.states[-1]
+    
+    return final_state
+
+def create_grid(sep):
+    x = np.arange(-2*sep,3*sep,sep)
+    xx,yy = np.meshgrid(x,x)
+    
+    mat = np.zeros([len(xx),len(xx)])
+    
+    for i in range(len(xx)):
+        for j in range(len(xx)):
+            mat[i,j] = (xx[i][j]**2 + yy[i][j]**2)
+            
+    return mat
+    
--- xygatesHamiltonian.py
+++ xygatesHamiltonian.py
@@ -18,7 +18,7 @@ import qutip.control.pulseoptim as cpo
 up = basis(2,0) # i.e. the column vector (1,0)
 down = basis(2,1) # i.e. the column vector (0,1)
 
-def H(delta,phi,omega = 65000000):
+def H(delta,phi,omega = 42000000):
     """
 
     Args:
@@ -40,7 +40,7 @@ delta_x = 0
 phi_x = 0
 w = 65000000 #omega same for y gate
 time = theta_x * 2 / w #time same for y gate
-time_array = np.linspace(0,time,1000)
+time_array = np.linspace(0,time,10000)
 print(time_array)
 
 xgate = H(delta_x, phi_x)
@@ -64,4 +64,49 @@ print(final_y.states)
 #final state
 print(final_x.states[-1])
 print(final_y.states[-1])
+
+#function to do example above automatically
+def evolution_H(initial, delta, phi,  omega = 42000000, timestep = 1000):
+    """
+
+    Args:
+        initial: Qobj. Initial state / ket
+        delta: detuning
+        phi: rotation axis / radians (pi/2 for y and 0 for x axis)
+        omega: angle of rotation
+        timestep: desired time step to calculate the time evolution / s
+
+    Returns:
+
+    """
+
+
+    Hamiltonian = H(delta,phi,omega)
+    time = theta_x * 2 / omega
+
+    time_array = np.linspace(0, time, timestep)
+    results = mesolve(Hamiltonian, initial, time_array)
+    final_state = results.states[-1]
+
+    return final_state
+
+
+#FIDELITIES
+
+#define the mixed state
+plus_state = (basis(2,0)+basis(2,1))/np.sqrt(2)
+fidelity_x = fidelity(plus_state * plus_state.dag(), evolution_H(up,0,0) * evolution_H(up,0,0).dag())
+print(fidelity_x)
+
+fidelity_y = fidelity(plus_state * plus_state.dag(), evolution_H(up,0,np.pi/2) * evolution_H(up,0,np.pi/2).dag())
+print(fidelity_y)
+
+
+#DRIFT due to our PERMANENT MAGNET
+g = 2
+mu = 1.760 * (10)**11 #gyromagnetic ratio electron
+delta = g * mu * deltaB
+initial = basis(2,0)
+
+Hamiltonian = H(delta, np.pi)
 
